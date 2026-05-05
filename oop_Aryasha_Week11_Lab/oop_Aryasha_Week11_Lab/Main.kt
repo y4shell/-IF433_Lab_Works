9bd708eb53edb414ce66e3439934ec8552670401
y4shell <mahabillahh@gmail.com>
2026-05-05 14:53:24 +0700

    Delete oop_Aryasha_Week11_Lab directory

--- oop_Aryasha_Week11_Lab/oop_Aryasha_Week11_Lab/Main.kt
+++
@@ -1,9 +0,0 @@
-package oop_Aryasha_Week11_Lab
-
-fun main() {
-    println("=== TEST EXTENSION FUNCTIONS ===")
-    println("Alex".addGreeting())
-    println("Hi".repeatTimes(3))
-    val text: String? = null
-    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
-}
--- oop_Aryasha_Week11_Lab/oop_Aryasha_Week11_Lab/SmartHomeModels.kt
+++
@@ -1,7 +0,0 @@
-package oop_Aryasha_Week11_Lab
-
-data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
-
-fun SmartDevice.diagnose(): String {
-    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
-}
